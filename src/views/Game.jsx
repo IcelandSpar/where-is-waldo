d15a3281f505d52a1ebbdc37accaf18e78d4d9ce
IcySpar <victorasielrodriguez@gmail.com>
2025-07-13 14:30:32 -0700

    feat: add func to update selected pixel on resize
    resizing window will update spot selected using
    relative proportion from previously recorded spot
    selected and image width
    
    width is determined through image width and height
    instead of page width/height

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -1,53 +1,56 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 
 import styles from "../styles/Game.module.css";
 import iSpy10 from "../assets/i_spy_10.jpg";
 
 const Game = () => {
-  const [ windowDimensions, setWindowDimensions ] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+
   const [pointClicked, setPointClicked] = useState(null);
   const [targetOptions, setTargetOptions] = useState({
-    targetWidth: 500,
-    targetDotSize: 50,
-    targetDotColor: "white",
-    reticleColor: "white",
-    reticleWidth: 35,
-    reticleStyle: "ridge",
+    targetWidth: 100,
+    targetDotSize: 10,
+    targetDotColor: "red",
+    reticleColor: "red",
+    reticleWidth: 10,
+    reticleStyle: "dashed",
   });
   const { difficulty } = useParams();
 
   useEffect(() => {
     const handleWindowResize = () => {
-      setWindowDimensions({
-        width: window.innerWidth,
-        height: window.innerHeight,
+      const image = document.getElementById('gameImage');
+      setPointClicked({
+        ...pointClicked,
+        xPageCoord: ((image.offsetWidth / pointClicked.windowWidth) * pointClicked.xPageCoord),
+        yPageCoord: ((image.offsetHeight / pointClicked.windowHeight) * pointClicked.yPageCoord),
+        windowWidth: image.offsetWidth,
+        windowHeight: image.offsetHeight,
       })
+
     };
 
     window.addEventListener('resize', handleWindowResize);
     return () => {
       window.removeEventListener('resize', handleWindowResize)
     }
-  }, [])
+  }, [pointClicked])
 
 
   const handleImageClick = (e) => {
-    console.log(e)
+    const image = document.getElementById('gameImage');
     setPointClicked({
       xPageCoord: e.pageX,
       yPageCoord: e.pageY,
+      windowWidth: image.offsetWidth,
+      windowHeight: image.offsetHeight,
     });
   };
 
   return (
     <>
       <main className={styles.gameMainCont}>
-        {console.log(windowDimensions)}
-        <img onClick={handleImageClick} className={styles.gameImage} src={iSpy10}></img>
+        <img id="gameImage" onClick={handleImageClick} className={styles.gameImage} src={iSpy10}></img>
         {pointClicked == null ? null : (
           <div
             className={styles.circleClicked}
@@ -72,6 +75,8 @@ const Game = () => {
                 height: `${targetOptions.targetDotSize}px`,
                 transform: `translateY(-50%) translateX(-50%)`,
                 backgroundColor: targetOptions.targetDotColor,
+                top: '50%',
+                left: '50%',
               }}
             ></div>
           </div>
