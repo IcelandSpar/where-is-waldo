cac9c3c046f4ce168615138b57d3b1ba94a96bfd
IcySpar <victorasielrodriguez@gmail.com>
2025-07-28 19:26:23 -0700

    feat: add loading and err msgs on home page fetch

--- src/views/partials/DifficultySelect.jsx
+++ src/views/partials/DifficultySelect.jsx
@@ -1,65 +1,85 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import styles from '../../styles/DifficultySelect.module.css';
+import styles from "../../styles/DifficultySelect.module.css";
 
 const DifficultySelect = () => {
   const [ gameImages, setGameImages ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ fetchErr, setFetchErr ] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
+    setIsLoading(true);
+    setFetchErr(false);
     fetch(`${import.meta.env.VITE_FETCH_BASE_URL}/game/images`, {
-      method: 'GET',
+      method: "GET",
     })
-    .then((res) => res.json())
-    .then((res) => {
-      setGameImages(res);
-    })
+      .then((res) => res.json())
+      .then((res) => {
+        setIsLoading(false);
+        setGameImages(res);
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err) {
+          setFetchErr(true);
+        };
+      })
   }, []);
 
   const handleGameSelectBtn = (e, game) => {
     e.preventDefault();
     console.log(game);
     // starts game and redirects to game
-    fetch(`${import.meta.env.VITE_FETCH_BASE_URL}/game/create-player/${game.image_id}`, {
-      method: 'POST',
-    })
-    .then((res) => res.json())
-    .then((res) => {
-          fetch(`${import.meta.env.VITE_FETCH_BASE_URL}/game/create-player-items/${game.image_id}/${res.player_id}`, {
-            method: 'POST'
-          })
+    fetch(
+      `${import.meta.env.VITE_FETCH_BASE_URL}/game/create-player/${
+        game.image_id
+      }`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        fetch(
+          `${import.meta.env.VITE_FETCH_BASE_URL}/game/create-player-items/${
+            game.image_id
+          }/${res.player_id}`,
+          {
+            method: "POST",
+          }
+        )
           .then((res) => res.json())
-          .then((res) => console.log(res))
-          navigate(`/${game.image_id}/${res.player_id}/${game.difficulty}`);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-   
-
-
-
-
-  }
+          .then((res) => console.log(res));
+        navigate(`/${game.image_id}/${res.player_id}/${game.difficulty}`);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <>
-    <h2>Choose an image</h2>
-    <ul className={styles.gameListUl}>
-    {gameImages == null ? null : (
-      gameImages.map((game, index) => {
-        return (
-          <li className={styles.gameListLi} key={game.image_id}>
-            <button onClick={(e) => handleGameSelectBtn(e, game)}>{game.image_name}</button>
-            <p>Difficulty: {game.difficulty}</p>
-          </li>
-        )
-      })
-    )}
-    </ul>
+      <h2>Choose a Game</h2>
+      {isLoading && !fetchErr ? <p>Loading Games...</p> : null}
+      {fetchErr ? <p>Something went wrong</p> : null}
+      {gameImages == null ? null : (
+        <ul className={styles.gameListUl}>
+          {gameImages.map((game, index) => {
+            return (
+              <li className={styles.gameListLi} key={game.image_id}>
+                <button onClick={(e) => handleGameSelectBtn(e, game)}>
+                  {game.image_name}
+                </button>
+                <p>Difficulty: {game.difficulty}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
-  )
+  );
 };
 
 export default DifficultySelect;
--- src/views/partials/Leaderboards.jsx
+++ src/views/partials/Leaderboards.jsx
@@ -4,49 +4,65 @@ import { format } from "date-fns";
 import styles from "../../styles/Leaderboards.module.css";
 
 const Leaderboards = () => {
-  const [leaderboard, setLeaderboard] = useState(null);
-  const [leaderboardLimit, setLeaderboardLimit] = useState(10);
+  const [ leaderboard, setLeaderboard ] = useState(null);
+  const [ leaderboardLimit, setLeaderboardLimit ] = useState(10);
+  const [ loadingLeaderboard, setLoadingLeaderboard ] = useState(false);
+  const [ fetchLeaderboardErr, setFetchLeaderboardErr ] = useState(false);
 
   useEffect(() => {
+    setLoadingLeaderboard(true);
     fetch(`${import.meta.env.VITE_FETCH_BASE_URL}/game/get-leaderboard`, {
       method: "GET",
     })
       .then((res) => res.json())
-      .then((res) => setLeaderboard(res));
+      .then((res) => {
+        setLeaderboard(res);
+        setLoadingLeaderboard(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setFetchLeaderboardErr(true);
+      });
   }, []);
 
   return (
     <>
-      <h3 className={styles.leaderboardHeading}>Leaderboard:</h3>
-      <div>
-      </div>
-      <table border={1}>
-        <caption>Top 10 of all images</caption>
-        <thead>
-          <tr>
-          <th>Player Name</th>
-          <th>Game Name</th>
-          <th>Difficulty</th>
-          <th>Date Completed</th>
-          <th>Time to complete</th>
-          </tr>
-        </thead>
-        <tbody>
-        {leaderboard == null ? null : (
-          leaderboard.map((playerRecord, indx) => {
-            return (
-              <tr key={playerRecord.player_id}>
-                <th>{playerRecord.name}</th>
-                <td>{playerRecord.image_name}</td>
-                <td>{playerRecord.difficulty}</td>
-                <td>{format(playerRecord.end_time, 'LLL-d-y')}</td>
-                <td>{parseFloat(playerRecord.difference).toFixed(2)} Seconds</td>
-              </tr>
-            )
-          })
-        )}
-        </tbody>
-      </table>
+      {loadingLeaderboard && !fetchLeaderboardErr != false ? (
+        <p>Loading Leaderboard...</p>
+      ) : null}
+      {!leaderboard ? null : (
+        <table border={1}>
+          <caption>Top 10 Players of all games</caption>
+          <thead>
+            <tr>
+              <th>Player Name</th>
+              <th>Game Name</th>
+              <th>Difficulty</th>
+              <th>Date Completed</th>
+              <th>Time to complete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leaderboard == null
+              ? null
+              : leaderboard.map((playerRecord, indx) => {
+                  return (
+                    <tr key={playerRecord.player_id}>
+                      <th>{playerRecord.name}</th>
+                      <td>{playerRecord.image_name}</td>
+                      <td>{playerRecord.difficulty}</td>
+                      <td>{format(playerRecord.end_time, "LLL-d-y")}</td>
+                      <td>
+                        {parseFloat(playerRecord.difference).toFixed(2)} Seconds
+                      </td>
+                    </tr>
+                  );
+                })}
+          </tbody>
+        </table>
+      )}
+    {fetchLeaderboardErr ? (<p>Something went wrong</p>): null}
+
     </>
   );
 };
